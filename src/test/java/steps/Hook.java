9225aba7f5a169740ad891f8bd0f66e12e175528
mudit-naithani <37511735+mudit-naithani@users.noreply.github.com>
2019-08-14 21:06:19 +0530

    test

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -32,8 +32,8 @@ public class Hook extends BaseUtil{
 
         //Chrome driver
         System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver");
-     //   base.Driver = new ChromeDriver();
-        webDriver driver = new ChromeDriver();
+        base.Driver = new ChromeDriver();
+      //  webDriver driver = new ChromeDriver();
     }
 
 
